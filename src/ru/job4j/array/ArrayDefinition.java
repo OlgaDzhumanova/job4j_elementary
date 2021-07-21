472fd0b929ea5aa7da376161b6bd8d840dd3f4e7
OlgaDzhumanova <dzhum-olga@yandex.ru>
2021-07-21 13:24:02 +0300

    Заполнение массива. [#226300]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -10,5 +10,14 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен: " + surnames.length);
         System.out.println("Размер массива равен: " + prices.length);
 
+        String[]names = new String[4];
+        names[0] = "Olga";
+        names[1] = "Maria";
+        names[2] = "Kristina";
+        names[3] = "Nastya";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
